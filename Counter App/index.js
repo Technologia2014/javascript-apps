bed39dad7872e323c393ba23f9de16307557ebee
Technologia2014 <eunicemaabenayayra@gmail.com>
2025-07-23 12:01:21 +0000

    commit counter

---
+++ Counter App/index.js
@@ -0,0 +1,21 @@
+
+document.getElementById('add').onclick = add;
+document.getElementById('reset').onclick = reset;
+document.getElementById('subtract').onclick = subtract;
+
+
+  var count;
+    
+function add(){
+  document.getElementById('display').textContent = count;
+    count += 1;
+}
+function reset(){
+    document.getElementById('display').textContent = count;
+       count = 0  
+}
+function subtract(){
+    document.getElementById('display').textContent = count;
+    count -= 1;
+    
+}
